a79d23074b9543a1aad2768c623834ac10621425
sathwik77 <sathwikkulkarni123@gmail.com>
2024-11-27 23:17:12 +0530

    changes are made in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('hi vishnu');
+    res.send('hello world');
 });
 
 app.listen(3000 , '0.0.0.0' , ()=>{
